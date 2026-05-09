e80ef4a3ae1d764e6e407a8fec792568ae6b2172
Rounak-T <thoratrounak27@gmail.com>
2026-05-10 01:36:06 +0530

    add skill gap analysis and matched skills to resume analysis endpoint

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, UploadFile, File, Form
 from app.utils.resume_parser import text_extracter
 from app.utils.text_cleaner import text_cleaner
 from app.utils.similarity import cosine_cal
+from app.utils.skill_extractor import skill_gap
 from fastapi.responses import JSONResponse
 
 app= FastAPI()
@@ -10,21 +11,17 @@ app= FastAPI()
 def test():
     return {"message": "The app is running"}
 
-# @app.post("/upload")
-# def TextExtracter(UserFile: UploadFile= File(...)):
-
-#     text= text_extracter(UserFile)
-#     text= text_cleaner(text)
-#     return JSONResponse(status_code=200, content={"text": text})
-
-@app.post("/match-score")
-def MatchScore(UserFile: UploadFile= File(...), raw_jd_text: str = Form(...)):
+@app.post("/resume-analysis")
+def ResumeAnalysis(UserFile: UploadFile= File(...), raw_jd_text: str = Form(...)):
 
     resume_text= text_extracter(UserFile)
-    resume_text= text_cleaner(resume_text)
 
+    resume_text= text_cleaner(resume_text)
     jd_text= text_cleaner(raw_jd_text)
 
+    skills= skill_gap(resume_text, jd_text)
+    missing_skills= skills['missing_skills']
+    matched_skills= skills['matched_skills']
     match_score= cosine_cal(resume_text, jd_text)
 
-    return match_score
+    return JSONResponse(status_code=200, content={"match_score": match_score, 'missing_skills': missing_skills, 'matched_skills': matched_skills})
---
+++ app/utils/skill_extractor.py
@@ -0,0 +1,29 @@
+import spacy
+from spacy.matcher import PhraseMatcher
+from skillNer.general_params import SKILL_DB
+from skillNer.skill_extractor_class import SkillExtractor
+
+nlp = spacy.load("en_core_web_lg")
+skill_extractor = SkillExtractor(nlp, SKILL_DB, PhraseMatcher)
+
+def extract_skills(text):
+
+    annotations = skill_extractor.annotate(text)
+    skills = list(set(
+    item['doc_node_value'].lower().strip()
+    for item in annotations['results']['ngram_scored']
+))
+
+    return skills
+
+def skill_gap(resume_text, jd_text):
+
+    resume_skills= set(extract_skills(resume_text))
+    jd_skills= set(extract_skills(jd_text))
+    missing_skills= jd_skills - resume_skills
+    matched_skills = jd_skills & resume_skills
+
+    return {
+        "matched_skills": list(matched_skills),
+        "missing_skills": list(missing_skills)
+    }
