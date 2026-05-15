37f1511831f5e597947894654c8696ee826fc7a0
Rounak-T <thoratrounak27@gmail.com>
2026-05-15 12:27:27 +0530

    "fix port to use $PORT env variable"

--- app/main.py
+++ app/main.py
@@ -92,4 +92,5 @@ def PredictRole(UserFile: UploadFile= File(...)):
 if __name__ == "__main__":
     import uvicorn
     import os
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
