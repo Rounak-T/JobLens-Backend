581cb85f1b377da7f1c537fc31c473725b7cddf9
Rounak-T <thoratrounak27@gmail.com>
2026-05-15 12:36:12 +0530

    fix port binding using PORT env variable

--- app/main.py
+++ app/main.py
@@ -92,5 +92,4 @@ def PredictRole(UserFile: UploadFile= File(...)):
 if __name__ == "__main__":
     import uvicorn
     import os
-    port = int(os.environ.get("PORT", 10000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
